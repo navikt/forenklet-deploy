71ee8f02f0eef5b8b5edf422d7421454de7dd851
Steffen Hageland <steffen.hageland@bekk.no>
2017-12-06 12:20:11 +0100

    bedre mockdata og fikser litt styling

--- src/frontend/src/mock/mock-ws.ts
+++ src/frontend/src/mock/mock-ws.ts
@@ -11,13 +11,29 @@ function createId(): string {
     return getRandomInt(1, 100000).toString();
 }
 
-function createEvent(application: string, environment: string, version: string): EventAction {
+function timeNow(): number {
+    return Date.now();
+}
+
+function timeMinutesAgo(min: number, max: number): number {
+    const date = new Date();
+    date.setUTCMinutes(date.getUTCMinutes() - getRandomInt(min, max));
+    return date.getTime();
+}
+
+function timeHoursAgo(): number {
+    const date = new Date();
+    date.setUTCHours(date.getUTCHours() - getRandomInt(1, 35));
+    return date.getTime();
+}
+
+function createEvent(application: string, environment: string, version: string, timestamp: number): EventAction {
     return {
         type: ActionType.EVENT,
         data: {
             id: createId(),
             eventType: 'mock',
-            timestamp: Date.now(),
+            timestamp,
             application,
             environment,
             version,
@@ -30,15 +46,15 @@ export function setupMockWS() {
     const mockServer = new Server('ws://localhost:8800/ws');
 
     mockServer.on('connection', (server: {}) => {
-        mockServer.send(JSON.stringify(createEvent('mockapp', 't6', '102.20170201.1224')));
-        mockServer.send(JSON.stringify(createEvent('mockapp', 'q6', '98.20170128.1043')));
-        mockServer.send(JSON.stringify(createEvent('mockapp', 'q0', '98.20170128.1043')));
-        mockServer.send(JSON.stringify(createEvent('mockapp', 'p', '98.20170128.1043')));
+        mockServer.send(JSON.stringify(createEvent('mockapp', 't6', '102.20170201.1224', timeNow())));
+        mockServer.send(JSON.stringify(createEvent('mockapp', 'q6', '98.20170128.1043', timeMinutesAgo(30, 50))));
+        mockServer.send(JSON.stringify(createEvent('mockapp', 'q0', '98.20170128.1043', timeMinutesAgo(3, 25))));
+        mockServer.send(JSON.stringify(createEvent('mockapp', 'p', '97.20170127.1134', timeHoursAgo())));
 
-        mockServer.send(JSON.stringify(createEvent('demoapp', 't6', '202.20170202.1356')));
-        mockServer.send(JSON.stringify(createEvent('demoapp', 'q6', '202.20170202.1356')));
-        mockServer.send(JSON.stringify(createEvent('demoapp', 'q0', '202.20170202.1356')));
-        mockServer.send(JSON.stringify(createEvent('demoapp', 'p', '202.20170202.1356')));
+        mockServer.send(JSON.stringify(createEvent('demoapp', 't6', '202.20170202.1356', timeNow())));
+        mockServer.send(JSON.stringify(createEvent('demoapp', 'q6', '202.20170202.1356', timeMinutesAgo(30, 50))));
+        mockServer.send(JSON.stringify(createEvent('demoapp', 'q0', '202.20170202.1356', timeMinutesAgo(3, 25))));
+        mockServer.send(JSON.stringify(createEvent('demoapp', 'p', '199.20170201.1206', timeHoursAgo())));
 
         mockServer.send(JSON.stringify({ type: 'EVENTS_PROVIDED', data: null}));
     });
