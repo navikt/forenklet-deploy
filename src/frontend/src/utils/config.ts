6733de1a527709376867ea6750f3a07a3e78a631
Steffen Hageland <steffen.hageland@nav.no>
2018-01-17 10:35:17 +0100

    Bruk riktig port på nais

--- src/frontend/src/utils/config.ts
+++ src/frontend/src/utils/config.ts
@@ -1,6 +1,7 @@
 const LOCAL_API_PORT = '8800';
 const hostname = window.location.hostname;
 const port = hostname === 'localhost' ? LOCAL_API_PORT : window.location.port;
+const portPart = port === '' ? '' : ':' + port;
 const protocol = window.location.protocol;
 
-export const apiBaseUri = `${protocol}://${hostname}:${port}/api`;
+export const apiBaseUri = `${protocol}://${hostname}:${portPart}/api`;
