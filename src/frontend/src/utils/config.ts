fa244c4902acbc2e27241e554c34d95f0e5e1353
Steffen Hageland <steffen.hageland@nav.no>
2018-01-17 10:49:24 +0100

    forenket config av api-base-url

--- src/frontend/src/utils/config.ts
+++ src/frontend/src/utils/config.ts
@@ -1,7 +1,3 @@
-const LOCAL_API_PORT = '8800';
-const hostname = window.location.hostname;
-const port = hostname === 'localhost' ? LOCAL_API_PORT : window.location.port;
-const portPart = port === '' ? '' : ':' + port;
-const protocol = window.location.protocol;
+const isLocalhost =  window.location.hostname === 'localhost';
 
-export const apiBaseUri = `${protocol}://${hostname}:${portPart}/api`;
+export const apiBaseUri = isLocalhost ? 'http://localhost:8800/api' : '/api';
