7a45eef8f478c181ac3f488e16b3b904441df4b0
Steffen Hageland <steffen.hageland@nav.no>
2018-01-17 10:32:52 +0100

    Bruk https på nais

--- src/frontend/src/utils/config.ts
+++ src/frontend/src/utils/config.ts
@@ -1,5 +1,6 @@
 const LOCAL_API_PORT = '8800';
 const hostname = window.location.hostname;
 const port = hostname === 'localhost' ? LOCAL_API_PORT : window.location.port;
+const protocol = window.location.protocol;
 
-export const apiBaseUri = `http://${hostname}:${port}/api`;
+export const apiBaseUri = `${protocol}://${hostname}:${port}/api`;
