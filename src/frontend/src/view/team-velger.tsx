059ef673a3693ee536a3c9bca3f16b6828a374bb
Steffen Hageland <steffen.hageland@nav.no>
2018-01-26 15:05:03 +0100

    Litt bedre håndtering av initial loading

--- src/frontend/src/view/team-velger.tsx
+++ src/frontend/src/view/team-velger.tsx
@@ -2,15 +2,13 @@ import * as React from 'react';
 import { AppState } from '../redux/reducer';
 import { connect, Dispatch } from 'react-redux';
 import { Action } from 'redux';
-import NavFrontendSpinner from 'nav-frontend-spinner';
 import { Select } from 'nav-frontend-skjema';
-import { selectIsLoadingTeams, selectTeams } from '../redux/team-duck';
+import { selectTeams } from '../redux/team-duck';
 import { Team } from '../models/team';
 import { velgTeam } from '../redux/team-velger-duck';
 import { getAllDeploys } from '../redux/deploy-duck';
 
 interface StateProps {
-    isLoadingData: boolean;
     teams: Team[];
 }
 
@@ -20,14 +18,9 @@ interface TeamVelgerDispatchProps {
 
 type TeamVelgerProps = StateProps;
 
-function TeamVelger({ isLoadingData, teams, doChangeValgtTeam}: TeamVelgerProps & TeamVelgerDispatchProps) {
-
+function TeamVelger({ teams, doChangeValgtTeam}: TeamVelgerProps & TeamVelgerDispatchProps) {
     function handleChange(event: any) {
         doChangeValgtTeam(event.target.value);
-    }
-
-    if (isLoadingData) {
-        return <NavFrontendSpinner />;
     }
 
     const options = teams.map((team) => <option value={team.id} key={team.id}>{team.displayName}</option>);
@@ -39,7 +32,6 @@ function TeamVelger({ isLoadingData, teams, doChangeValgtTeam}: TeamVelgerProps 
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
-    isLoadingData: selectIsLoadingTeams(state),
     teams: selectTeams(state),
 });
 
