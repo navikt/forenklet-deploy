51fb4c388f9d0d5a31eea2e96f0afe1d9fb62dd8
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-13 08:57:38 +0100

    Skriv om TeamAwareLink til å bli mer clean

--- src/frontend/src/view/team-aware-link.tsx
+++ src/frontend/src/view/team-aware-link.tsx
@@ -1,33 +1,26 @@
 import * as React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, NavLinkProps } from 'react-router-dom';
 import { connect, DispatchProp } from 'react-redux';
 import { AppState } from '../redux/reducer';
 import { selectValgtTeamId } from '../redux/team-velger-duck';
 
-interface HeaderStateProps {
+interface LinkStateProps {
     valgtTeamId: string;
 }
 
-interface LinkProps {
-    to: string;
-    className: string;
-    children: any;
-    valgtTeamId: string;
-}
+type TeamAwareLinkProps = LinkStateProps & NavLinkProps & DispatchProp<string>;
 
-function TeamAwareLink({to, className, children, valgtTeamId}: LinkProps) {
+function TeamAwareLink(props: TeamAwareLinkProps) {
+    const {children, valgtTeamId, to, dispatch, ...linkProps} = props;
+    const toWithTeam = `${to}?team=${valgtTeamId}`;
     return (
-        <NavLink to={`${to}?team=${valgtTeamId}`} className={className}>{children}</NavLink>
+        <NavLink to={toWithTeam} {...linkProps}>{children}</NavLink>
     );
 }
 
-interface TeamAwareAnchorProps {
-    valgtTeamId: string;
-}
+type TeamAwareAnchorProps = LinkStateProps & React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & DispatchProp<string>;
 
-type Props = TeamAwareAnchorProps & React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & DispatchProp<string>;
-
-function TeamAwareAnchor(props: Props) {
+function TeamAwareAnchor(props: TeamAwareAnchorProps) {
     const {children, valgtTeamId, dispatch, href, ...anchorProps} = props;
     const hrefWithTeam = `${href}?team=${valgtTeamId}`;
     return (
@@ -35,7 +28,7 @@ function TeamAwareAnchor(props: Props) {
     );
 }
 
-const mapStateToProps = (state: AppState): HeaderStateProps => ({
+const mapStateToProps = (state: AppState): LinkStateProps => ({
     valgtTeamId: selectValgtTeamId(state),
 });
 
