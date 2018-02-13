04dbcd1597f63c061c53cb904e11ebc8fe9862e6
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-13 08:44:34 +0100

    Skriv om signaturen til TeamAwareAnchor til å bli mer clean

--- src/frontend/src/view/team-aware-link.tsx
+++ src/frontend/src/view/team-aware-link.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { connect, DispatchProp } from 'react-redux';
 import { AppState } from '../redux/reducer';
 import { selectValgtTeamId } from '../redux/team-velger-duck';
 
 interface HeaderStateProps {
     valgtTeamId: string;
-}
-
-interface AnchorProps {
-    href: string;
-    valgtTeamId: string;
-    className?: string;
-    target?: string;
-    rel?: string ;
-    children?: any;
 }
 
 interface LinkProps {
@@ -30,9 +21,17 @@ function TeamAwareLink({to, className, children, valgtTeamId}: LinkProps) {
     );
 }
 
-function TeamAwareAnchor({href, valgtTeamId, children, className, target, rel}: AnchorProps) {
+interface TeamAwareAnchorProps {
+    valgtTeamId: string;
+}
+
+type Props = TeamAwareAnchorProps & React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & DispatchProp<string>;
+
+function TeamAwareAnchor(props: Props) {
+    const {children, valgtTeamId, dispatch, href, ...anchorProps} = props;
+    const hrefWithTeam = `${href}?team=${valgtTeamId}`;
     return (
-        <a href={`${href}?team=${valgtTeamId}`} className={className} target={target} rel={rel}>{children}</a>
+        <a href={hrefWithTeam} {...anchorProps}>{children}</a>
     );
 }
 
