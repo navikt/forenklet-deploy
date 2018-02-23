a87c045135e278f94c406e1ce9add344058c5944
Steffen Hageland <steffen.hageland@nav.no>
2018-02-23 15:45:07 +0100

    Legger til mer logging for oppsett av unleash

--- src/main/java/no/nav/fo/forenkletdeploy/service/UnleashService.java
+++ src/main/java/no/nav/fo/forenkletdeploy/service/UnleashService.java
@@ -3,17 +3,24 @@ package no.nav.fo.forenkletdeploy.service;
 import no.finn.unleash.DefaultUnleash;
 import no.finn.unleash.util.UnleashConfig;
 import no.nav.fo.forenkletdeploy.unleash.streategies.ByEnvironmentStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UnleashService {
     private DefaultUnleash unleash;
+    private Logger LOG = LoggerFactory.getLogger(UnleashService.class.getName());
 
     public UnleashService() {
+        String instanceId = System.getProperty("FASIT_ENVIRONMENT_NAME", "local");
+        String env = System.getProperty("UNLEASH_API_URL");
+        LOG.info(String.format("Setting up unleash with instance: '%s' with uri: '%s'", instanceId, env));
+
         UnleashConfig config = UnleashConfig.builder()
                 .appName("forenklet-deploy")
-                .instanceId(System.getProperty("FASIT_ENVIRONMENT_NAME", "local"))
-                .unleashAPI(System.getProperty("UNLEASH_API_URL"))
+                .instanceId(instanceId)
+                .unleashAPI(env)
                 .build();
 
         this.unleash = new DefaultUnleash(config, new ByEnvironmentStrategy());
