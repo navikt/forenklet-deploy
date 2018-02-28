58c92c015380ede882bbc7c5250b5002a427d7ce
Joakim Lindquister <joakim.lindquister@nav.no>
2018-02-28 13:14:40 +0100

    Bruk config-fil for applikasjonsporteføljen

--- src/main/java/no/nav/fo/forenkletdeploy/teams/TeamOppfolging.java
+++ src/main/java/no/nav/fo/forenkletdeploy/teams/TeamOppfolging.java
@@ -4,8 +4,15 @@ import no.nav.fo.forenkletdeploy.domain.ApplicationConfig;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+import static no.nav.json.JsonUtils.fromJson;
+import static no.nav.sbl.rest.RestUtils.withClient;
 
 public class TeamOppfolging implements Team {
+
+    public static final String APPLICATION_CONFIG_URL = "http://stash.devillo.no/projects/OPP/repos/team-oppfolging/raw/applikasjonsportefolje/config.json";
 
     @Override
     public String getId() {
@@ -24,22 +31,14 @@ public class TeamOppfolging implements Team {
 
     @Override
     public List<ApplicationConfig> getApplicationConfigs() {
-        return Arrays.asList(
-                ApplicationConfig.builder()
-                        .name("modiabrukerdialog")
-                        .gitUrl("ssh://git@stash.devillo.no:7999/dial/modiabrukerdialog.git")
-                        .build(),
-                ApplicationConfig.builder()
-                        .name("internarbeidsflatedecorator")
-                        .gitUrl("ssh://git@stash.devillo.no:7999/felles/internarbeidsflatedecorator.git")
-                        .build(),
-                ApplicationConfig.builder()
-                        .name("dialogstyring")
-                        .gitUrl("ssh://git@stash.devillo.no:7999/dial/dialogstyring.git")
-                        .build(),
-                ApplicationConfig.builder()
-                        .name("henvendelse")
-                        .gitUrl("ssh://git@stash.devillo.no:7999/dial/henvendelse.git")
-                        .build());
+        String json = withClient(c -> c.target(APPLICATION_CONFIG_URL).request().get(String.class));
+        Map<String, Map<String, String>> map = fromJson(json, Map.class);
+        return map.entrySet().stream()
+                .map(entry -> ApplicationConfig.builder()
+                        .name(entry.getKey())
+                        .gitUrl(entry.getValue().get("gitUrl"))
+                        .build()
+                )
+                .collect(toList());
     }
 }
