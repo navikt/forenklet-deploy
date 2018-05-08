6652817a104bc26476135b546ed7da1dd28ce434
Steffen Hageland <steffen.hageland@bekk.no>
2018-05-08 12:41:27 +0200

    Fjerner ubrukte domene-klasser

--- src/main/kotlin/no/nav/fo/forenkletdeploy/Domain.kt
+++ src/main/kotlin/no/nav/fo/forenkletdeploy/Domain.kt
@@ -1,13 +1,10 @@
 package no.nav.fo.forenkletdeploy
 
-import java.net.URL
 import java.time.ZonedDateTime
 
 data class ApplicationConfig(val name: String, val gitUrl: String)
 data class FeatureToggle(val name: String, val enabled: Boolean)
 data class Team(val id: String, val displayName: String, val jenkinsFolder: String)
-data class Application(val name: String, val version: String, val issueKeys: List<String>)
-data class ReleaseNote(val url: URL, val applications: List<Application>)
 
 data class VeraDeploy(
         val id: String = "",
@@ -46,12 +43,6 @@ data class JiraIssueFields(
         val status: JiraIssueStatus
 )
 data class JiraIssue(val id: String, val key: String, val fields: JiraIssueFields)
-data class JiraIssues(
-        val issues: List<JiraIssue>,
-        val maxResults: Int,
-        val startAt: Int,
-        val total: Int
-)
 
 data class TeamAppConfig(
         val gitUrl: String
