01a948afc0f7c230081d7f9de8ec26ad101ec1f8
Steffen Hageland <steffen.hageland@bekk.no>
2018-05-16 14:00:30 +0200

    Bedre feilhåndtering

--- src/main/kotlin/no/nav/fo/forenkletdeploy/rest/JiraIssueResource.kt
+++ src/main/kotlin/no/nav/fo/forenkletdeploy/rest/JiraIssueResource.kt
@@ -1,6 +1,7 @@
 package no.nav.fo.forenkletdeploy.rest
 
 import no.nav.fo.forenkletdeploy.service.JiraService
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 
 import javax.inject.Inject
@@ -9,9 +10,20 @@ import javax.inject.Inject
 @RequestMapping("/api/jira")
 class JiraIssueResource @Inject
 constructor(val jiraService: JiraService) {
+    val LOG = LoggerFactory.getLogger(JiraIssueResource::class.java)
+
     @GetMapping
     fun getJiraIssues(@RequestParam(name = "issue") issues: List<String>) =
-        issues.map { jiraService.getUserStory(issueId = it) }
+            issues
+                    .filter { !it.startsWith("kaizen", ignoreCase = true) }
+                    .mapNotNull {
+                        try {
+                            jiraService.getUserStory(issueId = it)
+                        } catch (ex: Exception) {
+                            LOG.error("Kunne ikke finne jira-issue $it", ex)
+                            null
+                        }
+                    }
 
     @GetMapping("/{issueid}")
     fun getJiraIssue(@PathVariable("issueid") issueid: String) =
