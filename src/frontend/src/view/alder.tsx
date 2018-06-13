8cb6fbd502652404980aef21c1903a5d3055491f
Steffen Hageland <steffen.hageland@bekk.no>
2018-06-13 15:49:40 +0200

    KAIZEN-0 Bytter fra moment til date-fns for dato-manipulering

--- src/frontend/src/view/alder.tsx
+++ src/frontend/src/view/alder.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
-import * as moment from 'moment';
-import 'moment/locale/nb';
-
-moment.locale('nb');
+import * as locale from 'date-fns/locale/nb';
+import * as formatDistance from 'date-fns/formatDistance';
 
 interface AlderProps {
     alder: number;
 }
 
 export function getAlder(age: number): string {
-    return moment(age).fromNow(true);
+    return formatDistance(new Date(age), Date.now(), { locale });
 }
 
 function Alder({alder}: AlderProps) {
