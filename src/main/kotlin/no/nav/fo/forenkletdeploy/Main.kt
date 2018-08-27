40267e76ae2b66572bf10120902775f5c3ee4d77
Steffen Hageland <steffen.hageland@nav.no>
2018-08-27 10:48:39 +0200

    Bedre oppsett for no-proxy

--- src/main/kotlin/no/nav/fo/forenkletdeploy/Main.kt
+++ src/main/kotlin/no/nav/fo/forenkletdeploy/Main.kt
@@ -21,8 +21,14 @@ open class Main
 fun main(args: Array<String>) {
     if ("true".equals(System.getProperty("webproxy.enabled", "true"), ignoreCase = true)) {
         val url = URL(getRequiredProperty("HTTP_PROXY"))
+        val noProxy = getRequiredProperty("NO_PROXY")
+                .split(",")
+                .joinToString("|") {
+                    if (it.startsWith(".")) "*$it" else it
+                }
+
         SSLUtil.turnOffSslChecking()
-        System.setProperty("http.nonProxyHosts", getRequiredProperty("NO_PROXY"))
+        System.setProperty("http.nonProxyHosts", noProxy)
         System.setProperty("http.proxyHost", url.host)
         System.setProperty("http.proxyPort", url.port.toString())
         System.setProperty("https.proxyHost", url.host)
