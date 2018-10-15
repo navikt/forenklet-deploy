315f1e0585292a2330b55e9315e777717d503658
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-10-15 12:29:46 +0200

    la til readyness-sjekk som sjekker at vi fungerer

--- src/main/kotlin/no/nav/fo/forenkletdeploy/rest/InternalResource.kt
+++ src/main/kotlin/no/nav/fo/forenkletdeploy/rest/InternalResource.kt
@@ -1,13 +1,31 @@
 package no.nav.fo.forenkletdeploy.rest
 
+import no.nav.fo.forenkletdeploy.service.ApplicationService
+import no.nav.fo.forenkletdeploy.service.GitService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.inject.Inject
 
 @RestController
 @RequestMapping("/internal")
-class InternalResource {
+class InternalResource @Inject
+constructor(
+        val applicationService: ApplicationService,
+        val gitService: GitService
+) {
+    private var ready = false
+
     @GetMapping("/isAlive")
     fun isAliveCheck(): String =
             "Application: UP"
+
+    @GetMapping("/isReady")
+    fun isReadyCheck(): String {
+        if (!ready) {
+            applicationService.getAllApplications().forEach({ a -> gitService.getTagsForApplication(a) })
+            ready = true
+        }
+        return "Application: READY"
+    }
 }
